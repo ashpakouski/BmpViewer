a7706f69498a83380a83a2102e5ee2762bd62518
Andrei Shpakovskiy <shpakovskiyandrei@gmail.com>
2023-07-18 16:49:34 +0300

    Implement memory allocation

--- BmpViewer.asm
+++ BmpViewer.asm
@@ -11,28 +11,45 @@ start:
         stdcall GetStdin
 
         invoke  CreateFileA, TestFile.path, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL
-        mov     [fileHandle], EAX
+        mov     [Handle.file], EAX
 
         cmp     EAX, INVALID_HANDLE_VALUE
         jne     noError
-        invoke  WriteConsole, [stdout], Error.cantOpenFile, Error.cantOpenFile_ - Error.cantOpenFile, NULL, NULL
-        invoke  WriteConsole, [stdout], TestFile.path, TestFile.path_ - TestFile.path, NULL, NULL
+        invoke  WriteConsole, [Handle.stdout], Error.cantOpenFile, Error.cantOpenFile_ - Error.cantOpenFile, NULL, NULL
+        invoke  WriteConsole, [Handle.stdout], TestFile.path, TestFile.path_ - TestFile.path, NULL, NULL
         jmp     exit
 noError:
 
+        invoke  GetFileSize, [Handle.file], NULL
+        mov     [Image.size], EAX
+
+        invoke  GetProcessHeap
+        mov     [Handle.processHeap], EAX
+
+        invoke  HeapAlloc, [Handle.processHeap], HEAP_ZERO_MEMORY, [Image.size]
+        mov     [Image.bytesPtr], EAX
+
+;        mov     EBX, 0
+;@@:
+;        invoke  SetConsoleTextAttribute, [stdout], EBX
+;        invoke  WriteConsole, [stdout], sampleText, sampleText_ - sampleText, NULL, NULL
+;        inc     EBX
+;        cmp     EBX, 256
+;        jne     @B
+
 exit:
-        invoke  ReadConsole, [stdin], lpBuffer, 1, lpCharsRead, NULL
+        invoke  ReadConsole, [Handle.stdin], lpBuffer, 1, lpCharsRead, NULL
         invoke  ExitProcess, 0
 
 proc    GetStdout
         invoke  GetStdHandle, STD_OUTPUT_HANDLE
-        mov     [stdout], EAX
+        mov     [Handle.stdout], EAX
         ret
 endp
 
 proc    GetStdin
         invoke  GetStdHandle, STD_INPUT_HANDLE
-        mov     [stdin], EAX
+        mov     [Handle.stdin], EAX
         ret
 endp
 
@@ -43,18 +60,30 @@ Error:
         .cantOpenFile   db      "Can't open file: "
         .cantOpenFile_:
 
+sampleText              db      " ",  219, 10
+sampleText_:
+
 
 TestFile:
-        .path           db      "file\path\person.bmp", 0
+        .path           db      "file\path\image.bmp", 0
         .path_:
 
 Const:
+title           db      "Pixel Viewer", 0
 lpBuffer        db      10 dup (0)
 lpCharsRead     dd      ?
 
-stdin           dd      ?
-stdout          dd      ?
-fileHandle      dd      ?
+Handle:
+        .stdin          dd      ?
+        .stdout         dd      ?
+        .file           dd      ?
+        .processHeap    dd      ?
+
+Image:
+        .size           dd      ?
+        .bytesPtr       dd      ?
+
+include 'Colors.asm'
 
 ; ======== Imports ========
 section         '.idata' import data readable
@@ -67,4 +96,8 @@ import          Kernel32,\
                 ReadConsole, 'ReadConsoleA',\
                 ExitProcess, 'ExitProcess',\
                 SetConsoleTitle, 'SetConsoleTitleA',\
-                CreateFileA, 'CreateFileA'
+                CreateFileA, 'CreateFileA',\
+                SetConsoleTextAttribute, 'SetConsoleTextAttribute',\
+                GetFileSize, 'GetFileSize',\
+                GetProcessHeap, 'GetProcessHeap',\
+                HeapAlloc, 'HeapAlloc'
