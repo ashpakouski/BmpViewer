7db95140fada808f3b092e18cd531052355bdfac
Andrei Shpakovskiy <shpakovskiyandrei@gmail.com>
2023-08-10 16:52:38 +0300

    Edit color table

---
+++ ColorTable.asm
@@ -0,0 +1,19 @@
+; Console colors (BGR) in order of their indexes
+ColorTable:
+        .black          db    12,  12,  12
+        .blue           db    218, 55,  0
+        .green          db    14,  161, 19
+        .aqua           db    221, 150, 58
+        .red            db    31,  15,  197
+        .purple         db    152, 23,  136
+        .yellow         db    0,   156, 193
+        .white          db    204, 204, 204
+        .gray           db    118, 118, 118
+        .lightBlue      db    255, 120, 59
+        .lightGreen     db    12,  198, 22
+        .lightAqua      db    214, 214, 97
+        .lightRed       db    86,  72,  231
+        .lightPurple    db    158, 0,   180
+        .lightYellow    db    165, 241, 249
+        .brightWhite    db    242, 242, 242
+ColorTable_:
